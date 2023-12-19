e8373d7bba60433803a7b996fdb428c50c2e10cc
vivan raj <vivanrajmittakodi6@gmail.com>
2023-12-19 14:12:51 +0530

    first commit

---
+++ pronote/src/App.jsx
@@ -0,0 +1,14 @@
+
+import React,{Components} from 'react';
+import './App.css';
+import Editor from './components/Editor'
+
+function App() {
+  return (
+    <div className="App">
+      <Editor/>
+    </div>
+  );
+}
+
+export default App;
---
+++ pronote/src/components/Editor.jsx
@@ -0,0 +1,34 @@
+// Editor.jsx
+import React, { Component } from 'react';
+import './Editor.css';
+
+class Editor extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({ value: e.target.value });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="input">
+                    <h3>Input</h3>
+                    <textarea className="input-text" onChange={this.handleChange} defaultValue={this.state.value} />
+                </div>
+                <div className="output">
+                    <h3>Pro Note</h3>
+                    <div className="output-text">{this.state.value}</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Editor;
